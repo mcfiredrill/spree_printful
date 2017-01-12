8f449b783254dc1e897839bc628dde9b2ff88a87
Lemuel Barango <lemuelbarango@Lemuels-Mac-mini.local>
2017-01-12 16:25:53 -0500

    WIP set up printfil attachment to vairant

--- app/controllers/spree/admin/printful_products_controller.rb
+++
@@ -1,5 +0,0 @@
-require 'printfulclient'
-class Spree::Admin::PrintfulProductsController < Spree::Admin::BaseController
-  def new
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin, path: Spree.admin_path do
-    resources :printful_products
     resource  :printful_settings, only: [:edit, :update]
   end
 end
