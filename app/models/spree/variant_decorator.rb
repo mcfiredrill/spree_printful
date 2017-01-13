0a322c75b79d4673cf9e64a035748c90f5e2594d
Lemuel Barango <lemuelbarango@Lemuels-Mac-mini.local>
2017-01-13 15:26:14 -0500

    changed has_one reference on variance to printful_printfile

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Variant.class_eval do
-  has_one :printful_printfile, as: :viewable, dependent: :destroy, class_name: "Spree::Image"
+  has_one :printful_printfile, as: :viewable, dependent: :destroy, class_name: "Spree::PrintfulPrintfile"
   accepts_nested_attributes_for :printful_printfile
 end
