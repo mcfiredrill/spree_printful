2d480c04db77baa65f94348f7abb86069fdcbfab
Lemuel Barango <lemmyb92@gmail.com>
2017-03-24 21:56:20 -0400

    fixed stock scheduler

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -6,34 +6,41 @@ Spree::Variant.class_eval do
   accepts_nested_attributes_for :printful_printfile
   validate :printful_has_variant
   validates :printful_printfile, presence: true, if: Proc.new { |variant| variant.printful_variant_id.present? }
+  after_create :update_printful_stock
 
   def is_printful_variant?
     self.printful_printfile.attachment? and self.printful_variant_id.present?
   end
 
-  def self.update_stock
-    braintree = Gateway::BraintreeVzeroBase.first.provider
-    result = { changed: 0, unchanged: 0 }
-    not_in_state(FINAL_STATES).find_each do |checkout|
-      checkout.state = braintree::Transaction.find(checkout.transaction_id).status
-      if checkout.state_changed?
-        result[:changed] += 1
-        checkout.save
+  def get_printful_variant_details
+    uri = URI.parse("http://api.printful.com")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new("/products/variant/#{self.printful_variant_id}")
+    http.request(request)
+  end
+
+  def update_printful_stock
+    stock_loc = Spree::StockLocation.where({name: 'printful'}).first_or_create
+    if is_printful_variant?
+      res = JSON.parse(get_printful_variant_details.body)
+      if(res['result']['variant']['in_stock'])
+        stock_items.where(stock_location: stock_loc).update(count_on_hand: 700000000)
       else
-        result[:unchanged] += 1
+        stock_items.where(stock_location: stock_loc).update(count_on_hand: 0)
       end
     end
-    result
+  end
+
+  def self.update_stock
+    Spree::Variant.where.not(printful_variant_id: nil).each do |vrnt|
+      vrnt.update_printful_stock
+    end
   end
 
   private
     def printful_has_variant
       if self.printful_variant_id
-        uri = URI.parse("http://api.printful.com")
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new("/products/variant/#{self.printful_variant_id}")
-        response = http.request(request)
-        case response
+        case get_printful_variant_details
         when Net::HTTPSuccess
           return
         else
