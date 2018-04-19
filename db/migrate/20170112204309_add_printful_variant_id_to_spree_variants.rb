df5830019260af16acfca45c28b0632c90f57bfb
Tony Miller <mcfiredrill@gmail.com>
2018-04-19 11:00:09 +0900

    add rails version to migration
    This is required in newer versions of rails. I believe the version
    number is supposed to be the lowest version that the migration is
    compatible with.

--- db/migrate/20170112204309_add_printful_variant_id_to_spree_variants.rb
+++ db/migrate/20170112204309_add_printful_variant_id_to_spree_variants.rb
@@ -1,4 +1,4 @@
-class AddPrintfulVariantIdToSpreeVariants < ActiveRecord::Migration
+class AddPrintfulVariantIdToSpreeVariants < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_variants, :printful_variant_id, :integer
   end
